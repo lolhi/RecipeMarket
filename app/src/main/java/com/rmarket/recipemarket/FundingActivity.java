b4458baa56adf8ceec74fcffe24bd20bcf986241
OPOv <qkrwlgns1207@gmail.com>
2019-08-08 01:11:37 +0900

    AddFundingDetail

--- app/src/main/java/com/rmarket/recipemarket/FundingActivity.java
+++ app/src/main/java/com/rmarket/recipemarket/FundingActivity.java
@@ -5,60 +5,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FundingActivity extends AppCompatActivity{
     Context mContext;
-    ImageView jadoo,apple,prefare1,prefare2;
-    ImageView back,basket;
+    ImageView fundingImage,fundingSearch;
+    LinearLayout fundingBtn;
+    ProgressBar progressBar;
+    TextView fundingDday, fundingPercent,fundingGoal,fundingPrecent,fundingUser;
     protected void onCreate(Bundle savedInstanceState) {
         mContext = this;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.shopping_gizo);
-        jadoo = findViewById(R.id.gizo_jadoo);
-        apple = findViewById(R.id.gizo_apple);
-        prefare1 = findViewById(R.id.gizo_prefare1);
-        prefare2 = findViewById(R.id.gizo_prefare2);
+        setContentView(R.layout.activity_funding);
 
-        back = findViewById(R.id.gizo_back);
-        basket = findViewById(R.id.gizo_basket);
-
-        back.setOnClickListener(new View.OnClickListener() {
+        fundingImage = findViewById(R.id.fundingImage);
+        fundingDday = findViewById(R.id.fundingDday);
+        fundingPercent = findViewById(R.id.fundingPercent);
+        fundingGoal = findViewById(R.id.fundingGoal);
+        fundingPrecent = findViewById(R.id.fundingPresent);
+        fundingUser = findViewById(R.id.fundingUser);
+        fundingBtn = findViewById(R.id.fundingBtn);
+        fundingSearch = findViewById(R.id.fundingSearch);
+        progressBar = findViewById(R.id.fundingProgress);
+        progressBar.setProgress(51);
+        fundingBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                finish();
+                Toast.makeText(mContext, "준비중입니다.", Toast.LENGTH_SHORT).show();
             }
         });
 
-        basket.setOnClickListener(new View.OnClickListener() {
+        fundingSearch.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(mContext, ActivityBasket.class);
+                Intent intent = new Intent(mContext, SearchRayout.class);
                 mContext.startActivity(intent);
-            }
-        });
-
-        jadoo.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(mContext, "김천 자두 준비중입니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        apple.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(mContext, "김천 사과 준비중입니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        prefare1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(mContext, "상품 준비중입니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        prefare2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(mContext, "상품 준비중입니다.", Toast.LENGTH_SHORT).show();
             }
         });
     }
