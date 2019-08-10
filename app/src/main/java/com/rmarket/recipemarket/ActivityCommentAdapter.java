3866dcb82e1419320d6ab092faa09366929b33d0
OPOv <qkrwlgns1207@gmail.com>
2019-08-10 18:39:56 +0900

    Add_Comment_Del

--- app/src/main/java/com/rmarket/recipemarket/ActivityCommentAdapter.java
+++ app/src/main/java/com/rmarket/recipemarket/ActivityCommentAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,6 +40,14 @@ public class ActivityCommentAdapter extends RecyclerView.Adapter<RecyclerView.Vi
         recyclerViewHolder.text.setText(item.getCommentText());
         recyclerViewHolder.time.setText(item.getCommetTime());
         recyclerViewHolder.name.setText(item.getCommentName());
+        recyclerViewHolder.del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(mContext, "position : "+position + " name : "+item.getCommentText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
         if(!item.getCommentProfile().equals("NoImg"))
             GlideApp.with(mContext).load(item.getCommentProfile()).into(recyclerViewHolder.profile);
     }
@@ -50,16 +59,19 @@ public class ActivityCommentAdapter extends RecyclerView.Adapter<RecyclerView.Vi
 
     public static class RecyclerViewHolder1 extends RecyclerView.ViewHolder {
        ImageView profile;
-       TextView name, time, text;
+       TextView name, time, text, del;
 
         public RecyclerViewHolder1(@NonNull View itemView) {
             super(itemView);
             profile = itemView.findViewById(R.id.comment_profile);
             profile.setBackground(new ShapeDrawable(new OvalShape()));
             profile.setClipToOutline(true);
+            del =itemView.findViewById(R.id.comment_del);
             name = itemView.findViewById(R.id.comment_name);
             time = itemView.findViewById(R.id.comment_time);
             text = itemView.findViewById(R.id.comment_text);
+
+
         }
     }
 }
